a6f111eda24ca3f18a9544390225be88627ce8db
baoyanjiu <lianlijunnw@126.com>
2020-12-11 16:45:05 +0800

    Add files via upload
    ADD:new project to test

---
+++ 66DevopsTest/66DevopsTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  66DevopsTest
+//
+//  Created by lian on 2020/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!~~~Hello MobileDevops")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 66DevopsTest/66DevopsTest/_6DevopsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6DevopsTestApp.swift
+//  66DevopsTest
+//
+//  Created by lian on 2020/12/11.
+//
+
+import SwiftUI
+
+@main
+struct _6DevopsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
